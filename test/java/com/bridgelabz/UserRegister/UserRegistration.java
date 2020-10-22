24488687702da9b24d6fba2670cbb26e8459d336
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 17:28:55 +0530

    Refactored : code for testing valid first name, second name, email id, phone number and password entry with happy or sad message

--- test/java/com/bridgelabz/UserRegister/UserRegistration.java
+++ test/java/com/bridgelabz/UserRegister/UserRegistration.java
@@ -13,46 +13,84 @@ public class UserRegistration
     }
     //Checking the validity of first name
     @Test
-    public void givenfirstName_WhenProper_ReturnTrue() {
+    public void givenfirstName_WhenProper_ReturnHappy() {
         UserValidator userValidator = new UserValidator();
-        boolean result;
+        String result;
         result = userValidator.validateFirstName("Kale");
-        Assert.assertTrue(result);
+        Assert.assertEquals("happy", result);
+    }
+    @Test
+    public void givenfirstName_WhenImpProper_ReturnSad() {
+        UserValidator userValidator = new UserValidator();
+        String result;
+        result = userValidator.validateFirstName("Ka");
+        Assert.assertEquals("sad", result);
     }
     //Checking the validity of second name
     @Test
-    public void givenLastName_WhenProper_ReturnTrue()
+    public void givenLastName_WhenProper_ReturnHappy()
     {
         UserValidator userValidator = new UserValidator();
-        boolean result = userValidator.validLastName("Ibrah");
-        Assert.assertTrue(result);
+       String result = userValidator.validLastName("Ibrah");
+       Assert.assertEquals("happy", result);
+    }
+    @Test
+    public void givenLastName_WhenImProper_ReturnSad()
+    {
+        UserValidator userValidator = new UserValidator();
+       String result = userValidator.validLastName("Ib");
+       Assert.assertEquals("sad", result);
     }
     //Checking the validity of email id
     @Test
-    public void givenEmail_WhenValid_ReturnTrue()
+    public void givenEmail_WhenValid_ReturnHappy()
     {
         UserValidator userValidator = new UserValidator();
-        boolean result;
+        String result;
         result = userValidator.validateEmailId("ikhale@gmail.co.in");
-        Assert.assertTrue(result);
+        Assert.assertEquals("happy", result);
+    }
+    @Test
+    public void givenEmail_WhenInValid_ReturnSad()
+    {
+        UserValidator userValidator = new UserValidator();
+        String result;
+        result = userValidator.validateEmailId("ikhale@gmail.coin");
+        Assert.assertEquals("sad", result);
     }
     //Checking the validity of mobile number
     @Test
-    public void givenMobileNumber_WhenValid_ReturnTrue()
+    public void givenMobileNumber_WhenValid_ReturnHappy()
     {
         UserValidator userValidator = new UserValidator();
-        boolean result;
+        String result;
         result = userValidator.validateMobileNumber("91 9745945143");
-        Assert.assertTrue(result);
+        Assert.assertEquals("happy", result);
+    }
+    @Test
+    public void givenMobileNumber_WhenInValid_ReturnSad()
+    {
+        UserValidator userValidator = new UserValidator();
+        String result;
+        result = userValidator.validateMobileNumber("919745945143");
+        Assert.assertEquals("sad", result);
     }
     //Check the validity of password
     @Test
-    public void givenPassword_WhenValid_ReturnTrue()
+    public void givenPassword_WhenValid_ReturnHappy()
     {
         UserValidator userValidator = new UserValidator();
-        boolean result;
+        String result;
         result = userValidator.validatePassword("ASDsde986#");
-        Assert.assertTrue(result);
+        Assert.assertEquals("happy", result);
+    }
+    @Test
+    public void givenPassword_WhenInValid_ReturnSad()
+    {
+        UserValidator userValidator = new UserValidator();
+        String result;
+        result = userValidator.validatePassword("ASDsde");
+        Assert.assertEquals("sad", result);
     }
 
 }
