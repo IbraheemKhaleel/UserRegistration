b16d04a064f7f4bb4f7010ce32bdbdc92393b981
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 20:21:00 +0530

    Refactored : code for custom messages of exceptions to invalid entries

--- main/java/com/bridgelabz/UserRegister/UserValidator.java
+++ main/java/com/bridgelabz/UserRegister/UserValidator.java
@@ -9,39 +9,68 @@ public class UserValidator
     public static final String MOBILE_NUMBER_PATTERN = "^[1-9][0-9][ ][6-9][0-9]{9}$";
     private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])" +  "(?=.*[!@#$%^&*]){1}" + "(?=.*[0-9]).{8,}$";
 
-    public String validateFirstName(String fname)
+    public String validateFirstName(String fname) throws Exception
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
+        try {
         if (pattern.matcher(fname).matches())
         	return "happy";
+        }catch(Exception e)
+        {
+        	throw new userRegistartionException("Please enter correct first name" );
+        }
         return "sad";
     }
-    public String validLastName(String lname)
+    public String validLastName(String lname) throws Exception
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
-        if (pattern.matcher(lname).matches())
+        try
+        {
+        	if (pattern.matcher(lname).matches())
         	return "happy";
+        } catch (Exception e)
+        {
+        	throw new userRegistartionException(" Please enter correct second name " );
+        }
         return "sad";
     }
-    public String validateEmailId(String email)
+    public String validateEmailId(String email) throws Exception
     {
         Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
-        if (pattern.matcher(email).matches())
+        try
+        {
+        	if (pattern.matcher(email).matches())
         	return "happy";
+        } catch (Exception e)
+        {
+        	throw new userRegistartionException("Please enter correct email" );
+        }
         return "sad";
     }
-    public String validateMobileNumber(String mobile)
+    public String validateMobileNumber(String mobile) throws Exception
     {
         Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
-        if(pattern.matcher(mobile).matches())
+        try
+        {
+        	if (pattern.matcher(mobile).matches())
         	return "happy";
+        } catch (Exception e)
+        {
+        	throw new userRegistartionException("Please enter correct mobile number" );
+        }
         return "sad";
     }
-    public String validatePassword(String password)
+    public String validatePassword(String password) throws Exception
     {
         Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-        if ( pattern.matcher(password).matches())
+        try
+        {
+        	if (pattern.matcher(password).matches())
         	return "happy";
+        } catch (Exception e)
+        {
+        	throw new userRegistartionException("Please enter correct password" );
+        }
         return "sad";
     }
 
