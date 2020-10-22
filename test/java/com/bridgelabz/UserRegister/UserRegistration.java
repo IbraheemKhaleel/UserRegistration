2c4d28d0a74708a774e7370ca4719c237af85687
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 13:27:32 +0530

    Added : code forbothe  first name and second name entry validation

--- test/java/com/bridgelabz/UserRegister/UserRegistration.java
+++ test/java/com/bridgelabz/UserRegister/UserRegistration.java
@@ -6,21 +6,25 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	Scanner input = new Scanner(System.in);
-	static final String FIRSTNAME_PATTERN = "^[A-Z]+[a-zA-Z]{2,}$";
-	//Method for first name entry validation
-	public void firstNameEntry()
+	static final String NAME_PATTERN = "^[A-Z]+[a-zA-Z]{2,}$";
+	//Method for first name and second name entry validation
+	public void NameEntry()
     {
         System.out.println("Enter your first name ");
+        System.out.println("Enter your second name");
         String firstName = input.next();
-        Pattern pattern = Pattern.compile(FIRSTNAME_PATTERN );
-        Matcher match = pattern.matcher(firstName);
-        System.out.println(firstName + "  " + match.matches());
+        String secondName = input.next();
+        Pattern pattern = Pattern.compile(NAME_PATTERN );
+        Matcher firstNameMatch = pattern.matcher(firstName);
+        Matcher secondNameMatch = pattern.matcher(secondName);
+        System.out.println(firstName + "  " + firstNameMatch.matches());
+        System.out.println(secondName + " " + secondNameMatch.matches());
     }
-	
+	//Main function
 	public static void main(String[] args) {
 		System.out.println(" Welcome to user registration program ");
 		UserRegistration userRegistration = new UserRegistration();
-		userRegistration.firstNameEntry();
+		userRegistration.NameEntry();
 	}
 
 }
