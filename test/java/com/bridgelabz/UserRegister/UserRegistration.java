db96abbdc6913bad6f54fd0167e86303d27d1e31
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 13:41:14 +0530

    Added : code for mobile phone entry valid

--- test/java/com/bridgelabz/UserRegister/UserRegistration.java
+++ test/java/com/bridgelabz/UserRegister/UserRegistration.java
@@ -7,6 +7,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	Scanner input = new Scanner(System.in);
 	static final String NAME_PATTERN = "^[A-Z]+[a-zA-Z]{2,}$";
+	static final String  EMAIL_PATTERN = "^[a-zA-Z.]{3,}[@][a-zA-Z]{2}[.][a-z]{2}[.][a-z]{2}$";
+	static final String PHONENUMBER_PATTERN = "^[1-9][0-9][ ][6-9][0-9]{9}$";
 	//Method for first name and second name entry validation
 	public void NameEntry()
     {
@@ -25,12 +27,20 @@ public class UserRegistration {
 	{
 	    System.out.println("Enter your email ");
 	    String email = input.next();
-	    String emailRegex = "^[a-zA-Z.]{3,}[@][a-zA-Z]{2}[.][a-z]{2}[.][a-z]{2}$";
 	    Pattern pattern;
-	    pattern = Pattern.compile(emailRegex);
+	    pattern = Pattern.compile(EMAIL_PATTERN);
 	    Matcher match = pattern.matcher(email);
 	    System.out.println(email + "  " + match.matches());
 	}
+	// method for entry of mobile number
+    public void mobileNumberEntry()
+    {
+        System.out.println("Enter your 10 digit mobile number with country code followed by space ");
+        String mobileNumber = input.next();
+        Pattern pattern = Pattern.compile(PHONENUMBER_PATTERN);
+        Matcher match = pattern.matcher(mobileNumber);
+        System.out.println(mobileNumber + "  " + match.matches());
+    }
 	//Main function
 	public static void main(String[] args) 
 	{
