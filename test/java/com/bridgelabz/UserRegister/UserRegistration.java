9f61f6381d73162e7a054a32b8483c499e076c6e
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 16:54:25 +0530

    Added : code for password with minimum 8 characters, atleast one numeric ,one upper case and one special character entry valid

--- test/java/com/bridgelabz/UserRegister/UserRegistration.java
+++ test/java/com/bridgelabz/UserRegister/UserRegistration.java
@@ -7,10 +7,11 @@ import java.util.regex.Pattern;
 public class UserRegistration 
 {
 	Scanner input = new Scanner(System.in);
-	static final String NAME_PATTERN = "^[A-Z]+[a-zA-Z]{2,}$";
-	static final String  EMAIL_PATTERN = "^[a-zA-Z.]{3,}[@][a-zA-Z]{2}[.][a-z]{2}[.][a-z]{2}$";
-	static final String PHONENUMBER_PATTERN = "^[1-9][0-9][ ][6-9][0-9]{9}$";
-	static final String PASSWORD_PATTERN = "^(?=.*[A-Z])" + "(?=.*[0-9]).{8,}$";
+	static final String NAME_PATTERN = "^[A-Z]+[a-zA-Z]{2,}$";    //name pattern for valid name
+	static final String  EMAIL_PATTERN = "^[a-zA-Z.]{3,}[@][a-zA-Z]{2}[.][a-z]{2}[.][a-z]{2}$";		//valid email pattern
+	static final String PHONENUMBER_PATTERN = "^[1-9][0-9][ ][6-9][0-9]{9}$";	// valid phone number pattern
+	static final String PASSWORD_PATTERN ="^(?=.*[A-Z])" +  "(?=.*[!@#$%^&*]){1}" + "(?=.*[0-9]).{8,}$";	//valid password pattern
+	
 	//Method for first name and second name entry validation
 	public void NameEntry()
     {
@@ -38,14 +39,15 @@ public class UserRegistration
     public void mobileNumberEntry()
     {
         System.out.println("Enter your 10 digit mobile number with country code followed by space ");
-        String mobileNumber = input.next();
+        String mobileNumber = "91 9878987876";
         Pattern pattern = Pattern.compile(PHONENUMBER_PATTERN);
         Matcher match = pattern.matcher(mobileNumber);
         System.out.println(mobileNumber + "  " + match.matches());
     }
+    //method for entry of password
     public void passwordEntry()
     {
-        System.out.println("Enter your password with minimum 8 alphabets and one upper cases ");
+        System.out.println("Enter your password with minimum 8 alphabets ,one numeric,one special character and one upper cases ");
         String password = input.next();     
         Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
         Matcher match = pattern.matcher(password);
