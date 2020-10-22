8d46cc98a8fd31eec20f593c9063e13571269802
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 13:33:10 +0530

    Added : code for email entry validation

--- test/java/com/bridgelabz/UserRegister/UserRegistration.java
+++ test/java/com/bridgelabz/UserRegister/UserRegistration.java
@@ -11,8 +11,8 @@ public class UserRegistration {
 	public void NameEntry()
     {
         System.out.println("Enter your first name ");
-        System.out.println("Enter your second name");
         String firstName = input.next();
+        System.out.println("Enter your second name");
         String secondName = input.next();
         Pattern pattern = Pattern.compile(NAME_PATTERN );
         Matcher firstNameMatch = pattern.matcher(firstName);
@@ -20,11 +20,24 @@ public class UserRegistration {
         System.out.println(firstName + "  " + firstNameMatch.matches());
         System.out.println(secondName + " " + secondNameMatch.matches());
     }
+	//Method for email entry validation
+	public void emailEntry()
+	{
+	    System.out.println("Enter your email ");
+	    String email = input.next();
+	    String emailRegex = "^[a-zA-Z.]{3,}[@][a-zA-Z]{2}[.][a-z]{2}[.][a-z]{2}$";
+	    Pattern pattern;
+	    pattern = Pattern.compile(emailRegex);
+	    Matcher match = pattern.matcher(email);
+	    System.out.println(email + "  " + match.matches());
+	}
 	//Main function
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println(" Welcome to user registration program ");
 		UserRegistration userRegistration = new UserRegistration();
 		userRegistration.NameEntry();
+		userRegistration.emailEntry();
 	}
 
 }
