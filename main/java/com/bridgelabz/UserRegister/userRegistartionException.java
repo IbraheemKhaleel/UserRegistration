329eeb68c23466e558e5fda0c9aeb04524168f26
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 20:20:14 +0530

    Added : code for creating class for custom messages of exceptions to invalid entries

---
+++ main/java/com/bridgelabz/UserRegister/userRegistartionException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.UserRegister;
+
+public class userRegistartionException extends Exception {
+	public userRegistartionException (String message)
+	{
+		super(message);
+	}
+
+}
